d38b7ff874774598f55ef96f01825a463c4bb06d
KarthikArunkumar <krthk.arvnd@gmail.com>
2016-10-27 18:03:55 +0530

    Test Application
    Simple test

---
+++ Hello Wrold/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hello_Wrold.Startup))]
+namespace Hello_Wrold
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
