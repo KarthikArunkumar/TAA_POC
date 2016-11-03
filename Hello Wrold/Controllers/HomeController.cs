a25d6e4e574d5b78305fe9829c4863927b53069b
KarthikArunkumar <krthk.arvnd@gmail.com>
2016-11-03 16:48:44 +0530

    Update HomeController.cs

--- Hello Wrold/Controllers/HomeController.cs
+++ Hello Wrold/Controllers/HomeController.cs
@@ -9,9 +9,7 @@ namespace Hello_Wrold.Controllers
     public class HomeController : Controller
     {
            public ActionResult Index()
-        {
-            return View();
-        }
+      
    
 
         public ActionResult About()
