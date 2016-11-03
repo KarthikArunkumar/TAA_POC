f531db92dd28b84528a850cee5c34026757e0aa6
KarthikArunkumar <krthk.arvnd@gmail.com>
2016-11-03 16:44:30 +0530

    Update HomeController.cs

--- Hello Wrold/Controllers/HomeController.cs
+++ Hello Wrold/Controllers/HomeController.cs
@@ -8,7 +8,12 @@ namespace Hello_Wrold.Controllers
 {
     public class HomeController : Controller
     {
-hi
+           public ActionResult Index()
+        {
+            return View();
+        }
+   
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
